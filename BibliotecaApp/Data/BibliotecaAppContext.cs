b3c3848e2e999c4bfbffcdc1a42cae78b91eefee
Luis Paulo Lessa <luis.junior@educacao.senac.rio>
2023-10-17 21:59:47 -0300

    Adicionar arquivos de projeto.

---
+++ BibliotecaApp/Data/BibliotecaAppContext.cs
@@ -0,0 +1,33 @@
+﻿using BibliotecaApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+
+namespace BibliotecaApp.Data
+{
+    public class BibliotecaAppContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public BibliotecaAppContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Livro> Livro { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var dbSettings = _configuration.GetSection("DatabaseSettings");
+
+            var connectionString = new SqlConnectionStringBuilder
+            {
+                DataSource = dbSettings["Server"],
+                InitialCatalog = dbSettings["Database"],
+                IntegratedSecurity = bool.Parse(dbSettings["TrustedConnection"]!),
+                MultipleActiveResultSets = bool.Parse(dbSettings["MultipleActiveResultSets"]!)
+            };
+
+            optionsBuilder.UseSqlServer(connectionString.ToString());
+        }
+    }
+}
---
+++ BibliotecaApp/Models/Livro.cs
@@ -0,0 +1,13 @@
+﻿namespace BibliotecaApp.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = default!;
+        public string Autor { get; set; } = default!;
+        public int AnoPublicacao { get; set; }
+        public string ISBN { get; set; } = default!;
+        public int QuantidadeDisponivel { get; set; }
+
+    }
+}
